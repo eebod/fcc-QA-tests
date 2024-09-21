71a22820c15f506da2b17747b4cd414ef43c6b00
eebod <peter.3bode@gmail.com>
2024-09-21 17:52:04 +0100

    Test One

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,73 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  const invalidNum = "invalid number";
+  const invalidUnit = "invalid unit";
+
+  this.getNum = function (input) {
+    if (["kg", "lbs", "gal", "mi", "km", "l", "L"].includes(input)) return 1;
+    if (/\//g.test(input)) {
+      if (/\d.*\/\d.*(?=\/\d.*)/g.test(input) || /\/\//g.test(input)) return invalidNum;
+      let [num, denom] = input.split("/").map(a => parseFloat(a));
+      return parseFloat((num / denom).toFixed(5));
+    } else {
+      let result = input.split("").filter(a => a < "A").join("");
+      if (/^\d+\.{0,1}\d+$/g.test(result) || !/(?=\d{2})/g.test(result)) return parseFloat(result);
+      return invalidNum;
+    }
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    let result = input.split("").filter(a => a >= "A").join("").toLowerCase();
+    if (["kg", "lbs", "gal", "mi", "km"].includes(result)) return result;
+    if (result === "l") return "L";
+    return invalidUnit;
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+    switch (initUnit) {
+      case "gal": result = "L"; break;
+      case "L": result = "gal"; break;
+      case "lbs": result = "kg"; break;
+      case "kg": result = "lbs"; break;
+      case "mi": result = "km"; break;
+      case "km": result = "mi"; break;
+    }
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     let result;
-    
+    switch (unit) {
+      case "gal": result = "galons"; break;
+      case "L": result = "litres"; break;
+      case "km": result = "kilometers"; break;
+      case "mi": result = "miles"; break;
+      case "lbs": result = "pounds"; break;
+      case "kg": result = "kilograms"; break;
+    }
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+
     let result;
-    
-    return result;
+    switch (initUnit) {
+      case "gal": result = initNum * galToL; break;
+      case "L": result = initNum / galToL; break;
+      case "lbs": result = initNum * lbsToKg; break;
+      case "kg": result = initNum / lbsToKg; break;
+      case "mi": result = initNum * miToKm; break;
+      case "km": result = initNum / miToKm; break;
+    }
+    return parseFloat(parseFloat(result).toFixed(5));
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,34 @@
 'use strict';
 
-const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+  app.get('/api/convert', (req, res) => {
+    const convertHandler = new ConvertHandler();
+    const { input } = req.query;
+    const getNum = convertHandler.getNum(input);
+    const getUnit = convertHandler.getUnit(input);
 
+    if (getNum === 'invalid number' && getUnit === 'invalid unit') {
+      return res.status(200).send('invalid number and unit');
+    } else if (getNum === 'invalid number') {
+      return res.status(200).send('invalid number');
+    } else if (getUnit === 'invalid unit') {
+      return res.status(200).send('invalid unit');
+    } else {
+      const getReturnUnit = convertHandler.getReturnUnit(getUnit);
+      const spellOutUnit = convertHandler.spellOutUnit(getUnit);
+      const spellOutInitUnit = convertHandler.spellOutUnit(getReturnUnit);
+      const convert = convertHandler.convert(getNum, getUnit);
+      const getString = convertHandler.getString(getNum, spellOutUnit, convert, spellOutInitUnit);
+
+      return res.status(200).json({
+        initNum: getNum,
+        initUnit: getUnit,
+        returnNum: convert,
+        returnUnit: getReturnUnit,
+        string: getString
+      });
+    }
+  });
 };
